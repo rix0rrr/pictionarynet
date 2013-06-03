e81d536c2624074d4953432bc103f26bb367ece7
Tonni Tielens <tonnitielens@gmail.com>
2013-06-03 21:37:04 +0200

    Merged arbiter.js with drawing code

--- arbiter/arbiter.js
+++ arbiter/arbiter.js
@@ -15,11 +15,13 @@ app.get('/', function(req, res) {
 
 io.sockets.on('connection', function(socket) {
     socket.on('gameState', function(state) {
+    	state.players = [];
         socket.broadcast.emit('gameState', state);
     });
 });
 
 var players = new Array();
+var lines = new Array();
 var playerCount = 0;
 var words = [
 	"Superfluous",
@@ -30,17 +32,24 @@ var words = [
 	"Apple",
 	"Supercoder"
 	];
+
+/*
 setInterval(function() {
 	playerCount++;
 	var score = Math.floor(Math.random() * 100 + 1);
 	var guess = words[Math.floor(Math.random() * words.length)];
+	var line = new Object();
+	var width = 768;
+	var height = 1024;
+	line.x1 = Math.floor(Math.random() * width);
+	line.y1 = Math.floor(Math.random() * height);
+	line.x2 = Math.floor(Math.random() * width);
+	line.y2 = Math.floor(Math.random() * height);
 	players.push({ name: "Player " + playerCount, score: score, latestGuess: guess});
-	io.sockets.emit('gameState', { round: 1, drawing: { width: 768, height: 1024 }, players: players });
+	lines.push(line);
+	io.sockets.emit('gameState', { round: 1, drawing: { width: 768, height: 1024 , lines: lines }, players: players });
 }, 5000);
-
-setInterval(function() {
-	// Update canvas.
-}, 2000);
+*/
 
 var oneDay = 24 * 60 * 60 * 1000;
 app.use('/sketchboard', express.static('../sketchboard', { maxAge: oneDay }));
